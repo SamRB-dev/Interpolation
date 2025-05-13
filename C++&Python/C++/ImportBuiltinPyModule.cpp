8a132f567ea4734e57632d289637c54a2648d52a
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-05-13 11:26:38 +0200

    Enhance ImportBuiltinPyModule: add comments for clarity and improve code structure

--- C++&Python/C++/ImportBuiltinPyModule.cpp
+++ C++&Python/C++/ImportBuiltinPyModule.cpp
@@ -1,18 +1,34 @@
 #include <python3.13/Python.h>
 #include <iostream>
+
 int main(void) {
     Py_Initialize();
-    PyObject *pName, *pModule, *pFunc, *pResult;
+    PyObject *pModule, *pFunc, *pResult;
     pModule = PyImport_ImportModule("os");
+    // Check if the module was successfully imported
     if (pModule) {
         std::cout << "Module Imported" << std::endl;
+
+        // Get the "getcwd" function from the "os" module
         pFunc = PyObject_GetAttrString(pModule, "getcwd");
         std::cout << "getcwd detected" << std::endl;
+
+        // Call the "getcwd" function with no arguments
         pResult = PyObject_CallObject(pFunc, nullptr);
+
+        // Convert the result (Python string) to a C-style string
         const char* result = PyUnicode_AsUTF8(pResult);
+
+        // Print the current working directory
         std::cout << result << std::endl;
+
+        // Decrease reference counts for the module and function objects
         Py_DECREF(pModule);
         Py_DECREF(pFunc);
+    } else if (!pModule) {
+        // Print the Python error if the module failed to load
+        PyErr_Print();
+        std::cerr << "Failed to load module" << std::endl;
     }
     Py_Finalize();
     return 0;
