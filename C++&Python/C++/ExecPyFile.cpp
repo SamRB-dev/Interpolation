357378baa59f33900c30a1cd9510b7de46ba2360
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-05-13 13:07:38 +0200

    Refactor ExecPyFile: improve code structure and add comments for clarity

--- C++&Python/C++/ExecPyFile.cpp
+++ C++&Python/C++/ExecPyFile.cpp
@@ -1,5 +1,28 @@
 #include <python3.13/Python.h>
+#include <iostream>  
 
-int main(void) {
+int main() {
+    FILE* fp;  // File pointer to handle the Python script file
+
+    // Initialize the Python interpreter
+    Py_Initialize();
+
+    // Open the Python script file in read mode
+    fp = fopen("SomePython.py", "r");  
+    if (fp) {
+        // If the file is successfully opened, execute the Python script
+        PyRun_SimpleFile(fp, "SomePython.py");  
+
+        // Close the file after execution
+        fclose(fp);
+    } else {
+        // If the file fails to open, print an error message to the standard error stream
+        std::cerr << "Failed to open script.py" << std::endl;
+    }
+
+    // Finalize the Python interpreter to clean up resources
+    Py_Finalize();
+
+    // Return 0 to indicate successful program execution
     return 0;
 }
