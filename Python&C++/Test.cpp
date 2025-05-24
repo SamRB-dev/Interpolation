773143f0a7cd1de65d62f9573447cf81f7aaefd1
SamRB-dev <sadimrahmanbadhon@gmail.com>
2025-05-24 13:34:55 +0200

    Enhance Test module: add FuncWithArgs and Greeter class, update Python script to demonstrate new functionality

--- Python&C++/Test.cpp
+++ Python&C++/Test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <pybind11/pybind11.h>
 
 
@@ -7,6 +8,29 @@ void FuncWithoutArgs() {
     std::cout << "Function without arguments called." << std::endl;
 }
 
+void FuncWithArgs(double a, double b) {
+    // Function with arguments
+    std::cout << "Function with arguments called: a = " << a << ", b = " << b << std::endl;
+}
+
+class Greeter {
+public:
+    Greeter(const std::string& name) : name(name) {}
+
+    std::string greet() const {
+        return "Hello, " + name + "!";
+    }
+
+private:
+    std::string name;
+};
+
 PYBIND11_MODULE(Test, function) {
     function.def("FuncWithoutArgs", &FuncWithoutArgs, "A function that takes no arguments and prints a message.");
+
+    function.def("FuncWithArgs", &FuncWithArgs, "A function that takes two arguments and prints their values.");
+
+    pybind11::class_<Greeter>(function, "Greeter")
+        .def(pybind11::init<const std::string&>())
+        .def("greet", &Greeter::greet);
 }
--- Python&C++/Test.py
+++ Python&C++/Test.py
@@ -1,2 +1,5 @@
 import Test
 Test.FuncWithoutArgs()
+Test.FuncWithArgs(1, 2)
+greeter = Test.Greeter("SamRB-dev")
+print(greeter.greet())
